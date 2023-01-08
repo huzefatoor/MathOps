7a232a73c29b9d468dc19e676cbf9e5ab99b75c2
huzefatoor <huzefamdtoor@gmail.com>
2023-01-07 20:59:44 -0500

    Add Operations Classes

---
+++ MathOps.java
@@ -0,0 +1,21 @@
+public class MathOps extends Operations{
+
+    public static void main(String[] args){
+
+        int x = add(98, 7);
+
+        System.out.println(x);
+
+        int y = subtract(4, 7);
+        System.out.println(y);
+
+        int z = subtract(1, 0);
+        System.out.println(z);
+
+        int n = minus(-9, -3);
+        System.out.println(n);
+
+        int result = getNegativeNumber(-20, -4);
+        System.out.println(result);
+    }
+}
---
+++ Operations.java
@@ -0,0 +1,47 @@
+public class Operations {
+
+    public static int add(int a, int b) {
+        int sum = a+ b;
+
+        return sum;
+    }
+
+    public static double multiply(int a, int b) {
+
+        double multiplication = a * b;
+
+        return multiplication;
+    }
+
+    public static int subtract(int a, int b) {
+
+        int minus = 0;
+
+        if(a > b) {
+            minus = a - b;
+        }else {
+            minus = b - a;
+        }
+
+        return minus;
+    }
+
+    public static int getNegativeNumber(int a, int b) {
+
+        int minus = 0;
+
+        if(a < b) {
+            minus = a - b;
+        }else {
+            minus = b - a;
+        }
+
+        return minus;
+    }
+
+    public static int minus(int a, int b) {
+        int result = a - b;
+
+        return result;
+    }
+}
